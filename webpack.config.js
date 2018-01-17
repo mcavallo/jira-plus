88bd046282eb236d124527d8e83b985e5b06c038
Mariano Cavallo <mariano.cavallo@gmail.com>
2018-01-17 23:33:19 +0100

    Add webpack build notification and editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const AnyBarWebpackPlugin = require('anybar-webpack')
 const isProduction = (process.argv.indexOf('-p') !== -1)
 
 module.exports = {
@@ -70,6 +71,7 @@ module.exports = {
     ], {
       copyUnmodified: true
     }),
+    new AnyBarWebpackPlugin(),
     /*
     new UglifyJSPlugin({
       sourceMap: true,
